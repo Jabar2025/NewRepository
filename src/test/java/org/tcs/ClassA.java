d3676558d07b42591a1eee549a6afc2c3cabe4c5
Jabar2002 <ja9128969@gmail.com>
2025-03-16 20:05:24 +0530

    new git project

---
+++ src/test/java/org/tcs/ClassA.java
@@ -0,0 +1,13 @@
+package org.tcs;
+
+public class ClassA {
+private void lenovo() {
+System.out.println("lenovo");
+}
+private void vivo() {
+System.out.println("vivo");
+}
+private void oppo() {
+System.out.println("oppo");
+}
+}
